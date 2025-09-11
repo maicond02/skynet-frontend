331cf216092b45845e300a8285ec91be30cd3bd2
Maicon Alves <eumaicondouglas.md45@gmail.com>
2025-09-10 22:01:08 -0300

    Feat: add Dashboard layout, navbar, and content components

---
+++ app/components/Dashboard/Navbar.vue
@@ -0,0 +1,29 @@
+
+<template>
+    <div class="card">
+        <Menubar :model="items" />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    label: 'Dashboard',
+                    icon: 'pi pi-home'
+                },
+                {
+                    label: 'Incidentes',
+                    icon: 'pi pi-exclamation-triangle'
+                },
+                {
+                    label: 'Configurações',
+                    icon: 'pi pi-cog'
+                }
+            ]
+        };
+    }
+};
+</script>
